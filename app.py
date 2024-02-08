ecce6f82284ca1df7341d0b5d95e1eb4f9405f91
AhmedMazroua <126991045+AhmedMazroua@users.noreply.github.com>
2024-02-08 17:39:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
-def forerunner():
+def index():
     return render_template("index.html")
 
-@app.route("/index", methods=["POST", "GET"])
-def index():
+@app.route("/quizlet", methods=["POST", "GET"])
+def quizlet():
     return render_template("quizlet.html")
 
 if __name__ == "__main__":
